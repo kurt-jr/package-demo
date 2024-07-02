8da1f78a72741f7138790752d6ab89a665d8cd42
kurt-jr <nps3cs@virginia.edu>
2024-07-02 11:01:28 -0400

    yah

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup
-    name='MyPackageName',
+setup(
+    name='packageDemo',
     version='1.0.0',
-    url='https://github.com/mypackage.git',
-    author='Author Name',
-    author_email='author@gmail.com',
-    description='Description of my package',
+    url='https://github.com/kurt-jr/package-demo',
+    author='Kurt',
+    author_email='nps3cs@virginia,edu',
+    description='package with 100% accurate prediction',
     packages=['mypackage']
     license = 'MIT'
     
