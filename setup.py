ae0ffe30a3976af66cc049807792f185f0cd9e68
kurt-jr <nps3cs@virginia.edu>
2024-07-02 11:01:40 -0400

    bruh

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='packageDemo',
     version='1.0.0',
     url='https://github.com/kurt-jr/package-demo',
-    author='Kurt',
+    author='Kurt Samuels Jr',
     author_email='nps3cs@virginia,edu',
     description='package with 100% accurate prediction',
     packages=['mypackage']
